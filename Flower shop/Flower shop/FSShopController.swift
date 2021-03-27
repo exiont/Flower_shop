d2d98a0df756470a4f2032f2b1fc4670bc052e5b
Yauhen Khamitsevich <exiont@gmail.com>
2021-03-27 22:11:49 +0300

    ADD logo and label

--- Flower shop/Flower shop/FSShopController.swift
+++ Flower shop/Flower shop/FSShopController.swift
@@ -9,8 +9,34 @@ import UIKit
 
 class FSShopController: UIViewController {
 
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "flower_logo")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageView
+    }()
+
+    private lazy var appLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Цветочный магазин"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 35)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.view.addSubview(logoImageView)
+        self.view.addSubview(appLabel)
+    }
+
+    private func setupConstraints() {
+//        self.logoImageView.snp
     }
 }
